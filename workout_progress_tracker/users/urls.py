b5ed7c9bc3abd4b746c851f1af3201075b8d1b79
khabdrick <muhamzyali@gmail.com>
2021-01-29 18:39:00 +0100

    urls for user model

--- workout_progress_tracker/users/urls.py
+++ workout_progress_tracker/users/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"),name='login'),
+    path("login/", auth_views.LoginView.as_view(template_name="users/login.html"),name='login'),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("signup/", views.SignUp.as_view(), name="signup"),
 ]
