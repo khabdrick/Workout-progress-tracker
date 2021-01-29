cb20b309409b7312794a8558bcecc0100df740c1
khabdrick <muhamzyali@gmail.com>
2021-01-29 15:41:07 +0100

    removed secret key from app

--- workout_progress_tracker/workout_progress_tracker/settings.py
+++ workout_progress_tracker/workout_progress_tracker/settings.py
@@ -9,20 +9,16 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "h-@&ome+@h_lt)m6#9g6#=k=+k!id8z7+x8r-hao6yvli5^*_d"
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
