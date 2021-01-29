f7d1f9b7b1b08ac076691b277f7ea5de460424e0
khabdrick <muhamzyali@gmail.com>
2021-01-29 15:27:04 +0100

    added user model and form

--- workout_progress_tracker/users/models.py
+++ workout_progress_tracker/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib import auth
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(auth.models.User, auth.models.PermissionsMixin):
+    
+    def __str__(self):
+        return self.username
