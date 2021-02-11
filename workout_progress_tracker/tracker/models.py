269b9ff74b5326884a92a57104bf4bd9cba1ad35
khabdrick <muhamzyali@gmail.com>
2021-02-11 22:25:50 +0100

    made list of days

--- workout_progress_tracker/tracker/models.py
+++ workout_progress_tracker/tracker/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from users import User
 
+CHOICES = (
+    ("monday", "Monday"),
+    ("tuesday", "Tuesday"),
+    ("wednesday", "Wednesday"),
+    ("thursday", "Thursday"),
+    ("friday", "Friday"),
+    ("saturday", "Saturday"),
+    ("sunday", "Sunday"),
+    
+)
 
 class Exercise(models.Model):
     names=
