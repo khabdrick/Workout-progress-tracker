1b35a45b501bd64028a64b267c66fc1e20529636
khabdrick <muhamzyali@gmail.com>
2021-02-12 13:35:03 +0100

    added regime app

--- workout_progress_tracker/tracker/models.py
+++ workout_progress_tracker/tracker/models.py
@@ -24,16 +24,29 @@ class Exercise(models.Model):
 class Day(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
-    excercises = models.ForeignKey(Exercise,related_name="exercise", on_delete=models.CASCADE)
+    excercises1 = models.ForeignKey(Exercise,related_name="exercises1", on_delete=models.CASCADE)
+    excercises2 = models.ForeignKey(Exercise,related_name="exercises2", on_delete=models.CASCADE)
+    excercises3 = models.ForeignKey(Exercise,related_name="exercises3", on_delete=models.CASCADE)
+    excercises4 = models.ForeignKey(Exercise,related_name="exercises4", on_delete=models.CASCADE)
+    excercises5 = models.ForeignKey(Exercise,related_name="exercises5", on_delete=models.CASCADE)
+    excercises6 = models.ForeignKey(Exercise,related_name="exercises6", on_delete=models.CASCADE)
+    excercises7 = models.ForeignKey(Exercise,related_name="exercises7", on_delete=models.CASCADE)
+    excercises8 = models.ForeignKey(Exercise,related_name="exercises8", on_delete=models.CASCADE)
+    excercises9 = models.ForeignKey(Exercise,related_name="exercises9", on_delete=models.CASCADE)
+    excercises10 = models.ForeignKey(Exercise,related_name="exercises10", on_delete=models.CASCADE)
+
+
     body_parts = models.CharField(max_length=225)
 
-class WorkoutDailyLog:
-    current_date= models.DateField(
+class WorkoutDailyLog(models.Model):
+    day_of_the_week = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
+    current_date= models.DateTimeField(
         max_length=255,
         null=True,
         blank=True,
         auto_now_add=True
     )
-    exercise_reps= models.ForeignKey(WorkorkRegimen,)
+
+    exercise_reps= models.ForeignKey()
 
 
