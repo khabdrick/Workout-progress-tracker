0bc2ecbacc99a69de8c3dd89ba1316e035ad2448
khabdrick <muhamzyali@gmail.com>
2021-02-12 13:02:35 +0100

    rearranged the model;s

--- workout_progress_tracker/tracker/models.py
+++ workout_progress_tracker/tracker/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.db.models.base import Model
 from users.models import User
 
-CHOICES = (
+DAYS_OF_THE_WEEK = (
     ("monday", "Monday"),
     ("tuesday", "Tuesday"),
     ("wednesday", "Wednesday"),
@@ -17,20 +17,23 @@ class Exercise(models.Model):
     names=models.CharField(max_length=225)
     reps_goals=models.PositiveIntegerField(max_length=223)
     sets_goals= models.PositiveIntegerField(max_length=223)
+    max_weight=models.PositiveIntegerField(max_length=22)
 
-class WorkorkRegimen(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    day= models.CharField(max_length=225, choices=CHOICES)
-    excercise= models.ForeignKey(Exercise,related_name="exercise", on_delete=models.CASCADE)
-    body_part = models.CharField(max_length=225)
 
-# class WorkoutDailyLog:
-#     current_date= models.DateField(
-#         max_length=255,
-#         null=True,
-#         blank=True,
-#         auto_now_add=True
-#     )
-#     exercise_reps= models.ForeignKey(WorkorkRegimen,)
+
+class Day(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
+    excercises = models.ForeignKey(Exercise,related_name="exercise", on_delete=models.CASCADE)
+    body_parts = models.CharField(max_length=225)
+
+class WorkoutDailyLog:
+    current_date= models.DateField(
+        max_length=255,
+        null=True,
+        blank=True,
+        auto_now_add=True
+    )
+    exercise_reps= models.ForeignKey(WorkorkRegimen,)
 
 
