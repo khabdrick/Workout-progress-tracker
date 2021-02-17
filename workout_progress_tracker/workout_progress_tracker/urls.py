7d26c1d40f4ea5f32da1f2be1aeb405713eeafd6
khabdrick <muhamzyali@gmail.com>
2021-02-17 22:57:24 +0100

    organised root urls

--- workout_progress_tracker/workout_progress_tracker/urls.py
+++ workout_progress_tracker/workout_progress_tracker/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/", include("users.urls", namespace="users")),
-    # path("tracker/", include("accounts.urls", namespace="accounts")),
+    path("tracker/", include("tracker.urls", namespace="tracker")),
+    path("regimen/", include("regimen.urls", namespace="regimen")),
 ]
