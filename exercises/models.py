4f15a55faf2d0bcd6bc570c0701bfc8579b35973
khabdrick <muhamzyali@gmail.com>
2021-02-18 01:38:01 +0100

    created exercise model

--- exercises/models.py
+++ exercises/models.py
@@ -1,25 +1,28 @@
 from django.db import models
 from users.models import User
 
-class Exercise(models.Model):
-	# Attributes
-	name = models.CharField(null = False, blank = False, max_length = 100)
-	description = models.TextField(null = False, blank = True, max_length = 1000)
 
-	# Methods
-	def __str__(self):
-		return str( self.name)
+class Exercise(models.Model):
+    # Attributes
+    name = models.CharField(null=False, blank=False, max_length=100)
+    description = models.TextField(null=False, blank=True, max_length=1000)
+
+    # Methods
+    def __str__(self):
+        return str(self.name)
+
 
 class ExerciseSet(models.Model):
-	# Attributes
-	exercise = models.ForeignKey(Exercise, null = False, blank = False, on_delete = models.CASCADE)
-	sets = models.ManyToManyField('exercises.Set', related_name = 'sets')
-	number_of_sets = models.IntegerField(null = False, blank = False, default = 3)
+    # Attributes
+    exercise = models.ForeignKey(
+        Exercise, null=False, blank=False, on_delete=models.CASCADE
+    )
+    sets = models.ManyToManyField("exercises.Set", related_name="sets")
+    number_of_sets = models.IntegerField(null=False, blank=False, default=3)
 
-	# Methods
-	def __str__(self):
-		return str(self.exercise.name) + ' - ' + str(self.number_of_sets) + ' sets'
-
+    # Methods
+    def __str__(self):
+        return str(self.exercise.name) + " - " + str(self.number_of_sets) + " sets"
 
 
 DAYS_OF_THE_WEEK = (
@@ -34,15 +37,23 @@ DAYS_OF_THE_WEEK = (
 
 
 class Set(models.Model):
-	# Constants
-	REPS_UNIT = (('RE', 'Reps'), ('SE', 'Seconds'), ('UF', 'Until Failure'))
-	WEIGHT_UNIT = (('KG', 'Kg.'), ('BW', 'Body Weight'))
-	# Attributes
-	reps = models.PositiveIntegerField(null = False, blank = False, default = 0)
-	reps_unit = models.CharField(max_length = 2, choices = REPS_UNIT, default = 'RE')
-	weight = models.DecimalField(max_digits = 5, decimal_places = 2)
-	weight_unit = models.CharField(max_length = 2, choices = WEIGHT_UNIT, default = 'RE')
+    # Constants
+    REPS_UNIT = (("RE", "Reps"), ("SE", "Seconds"), ("UF", "Until Failure"))
+    WEIGHT_UNIT = (("KG", "Kg."), ("BW", "Body Weight"))
+    # Attributes
+    reps = models.PositiveIntegerField(null=False, blank=False, default=0)
+    reps_unit = models.CharField(max_length=2, choices=REPS_UNIT, default="RE")
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    weight_unit = models.CharField(max_length=2, choices=WEIGHT_UNIT, default="RE")
 
-	# Methods
-	def __str__(self):
-		return str(self.reps) + ' ' + str(self.reps_unit) + ' x ' + str(self.weight) + ' ' + str(self.weight_unit)
+    # Methods
+    def __str__(self):
+        return (
+            str(self.reps)
+            + " "
+            + str(self.reps_unit)
+            + " x "
+            + str(self.weight)
+            + " "
+            + str(self.weight_unit)
+        )
