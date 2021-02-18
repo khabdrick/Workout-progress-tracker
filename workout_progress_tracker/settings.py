bb7e243f30c9618fb62001c3272f7b93d7cbd30c
khabdrick <muhamzyali@gmail.com>
2021-02-18 02:12:57 +0100

    removed dotenv from settings

--- workout_progress_tracker/settings.py
+++ workout_progress_tracker/settings.py
@@ -11,9 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
