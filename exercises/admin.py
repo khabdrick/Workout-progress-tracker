36b761ffef03084300fd38132fc7c52628c9c658
khabdrick <muhamzyali@gmail.com>
2021-02-19 20:50:34 +0100

    added set to admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Exercise, ExerciseSet
+from .models import Exercise, ExerciseSet, Set
 
 admin.site.register(Exercise)
 admin.site.register(ExerciseSet)
+admin.site.register(Set)
+
