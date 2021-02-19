c633f24aecb054d22ee6dd2a4c339ff311ad8a00
khabdrick <muhamzyali@gmail.com>
2021-02-19 23:03:18 +0100

    created body part model

--- body/models.py
+++ body/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class BodyPart(models.Model):
+	# Attributes
+	name = models.CharField(null = False, blank = False, max_length = 100)
+
+	# Methods
+	def __str__(self):
+		return self.name
