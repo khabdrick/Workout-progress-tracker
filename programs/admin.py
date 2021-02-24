44f1085711c0979f37b77a295e2df767d3a2ac94
khabdrick <muhamzyali@gmail.com>
2021-02-24 13:08:54 +0100

    updated program admin

--- programs/admin.py
+++ programs/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Program
+from .models import Program, WorkoutDay, WorkoutSession
 
 admin.site.register(Program)
+admin.site.register(WorkoutDay)
+admin.site.register(WorkoutSession)
