176889ff3563c4ad7a8662b3a914b0297141e57a
khabdrick <muhamzyali@gmail.com>
2021-02-25 11:10:01 +0100

    added logs to settings

--- workout_progress_tracker/settings.py
+++ workout_progress_tracker/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
-    "tracker",
+    "logs",
     "programs",
     "exercises",
     "crispy_forms",
