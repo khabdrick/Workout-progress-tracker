0e96fa7503d5505a46ab277ec5545602d2d567a3
khabdrick <muhamzyali@gmail.com>
2021-02-26 20:32:59 +0100

    model for log completed

--- logs/models.py
+++ logs/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+DAYS_OF_THE_WEEK = (
+    ("monday", "Monday"),
+    ("tuesday", "Tuesday"),
+    ("wednesday", "Wednesday"),
+    ("thursday", "Thursday"),
+    ("friday", "Friday"),
+    ("saturday", "Saturday"),
+    ("sunday", "Sunday"),
+)
+
+
 class WorkoutLog(models.Model):
     TYPE = (
         ("BU", "Bulking"),
@@ -13,3 +24,6 @@ class WorkoutLog(models.Model):
     summary = models.TextField(null=False, blank=True, max_length=1500)
     goal = models.CharField(max_length=2, choices=TYPE, default="MA")
     day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
+    date = models.DateField(max_length=223, auto_now_add=True)
+    exercises = models.ManyToManyField("exercises.ExerciseSet")
+
