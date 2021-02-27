40112ef23cc7196ee9c0ce22d0a314d7525692bf
khabdrick <muhamzyali@gmail.com>
2021-02-27 19:29:13 +0100

    deleted program admin

--- programs/admin.py
+++ programs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Program, WorkoutDay, WorkoutSession
+from .models import  WorkoutDay, WorkoutSession
 
-admin.site.register(Program)
+
 admin.site.register(WorkoutDay)
 admin.site.register(WorkoutSession)
