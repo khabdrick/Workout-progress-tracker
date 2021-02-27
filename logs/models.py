faafee077262eaa8fd7a33a76231e9cebab61a5a
khabdrick <muhamzyali@gmail.com>
2021-02-27 11:44:45 +0100

    model for log migration

--- logs/models.py
+++ logs/models.py
@@ -20,7 +20,7 @@ class WorkoutLog(models.Model):
     )
 
     # Attributes
-    name = models.CharField(null=False, blank=False, max_length=100)
+    name = models.CharField(null=True, blank=True, max_length=100)
     summary = models.TextField(null=False, blank=True, max_length=1500)
     goal = models.CharField(max_length=2, choices=TYPE, default="MA")
     day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
