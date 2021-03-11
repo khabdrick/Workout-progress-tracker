4521a5a03d4bb3b9c6604524d55c0a9600690d4c
Muhammed Ali <muhamzyali@gmail.com>
2021-03-11 06:57:52 +0100

    fixed the manytomany field that involves body

--- logs/models.py
+++ logs/models.py
@@ -25,4 +25,4 @@ class WorkoutLog(models.Model):
     goal = models.CharField(max_length=2, choices=TYPE, default="MA")
     date = models.DateField(max_length=223, auto_now_add=True)
     exercises = models.ManyToManyField("exercises.ExerciseSet")
-    muscles = models.ManyToManyField("body.Muscle")
+    muscles = models.ManyToManyField("body.MuscleGroup")
