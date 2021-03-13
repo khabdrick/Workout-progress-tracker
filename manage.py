540a822c9e7c39dedfed2c42b37a992cdec9647d
Muhammed Ali <muhamzyali@gmail.com>
2021-03-13 20:24:53 +0100

    editted manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "workout_progress_tracker.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", 
+    "workout_progress_tracker.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
