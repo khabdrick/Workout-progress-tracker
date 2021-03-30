b12219957c2933b9a25c54512c8c97da71690e40
Muhammed Ali <muhamzyali@gmail.com>
2021-03-30 12:43:36 +0100

    added dropdown for muscle group

--- body/models.py
+++ body/models.py
@@ -2,8 +2,20 @@ from django.db import models
 
 
 class MuscleGroup(models.Model):
+    MUSCLE_GROUP = (
+        ("calves", "Calves"),
+        ("hamstrings", "Hamstrings"),
+        ("quads", "Quadriceps(quads)"),
+        ("glutes", "Glutes"),
+        ("biceps", "Biceps"),
+        ("triceps", "Triceps"),
+        ("forearms", "Forearms"),
+        ("traps", "Trapezius(traps)"),
+    )
     # Attributes
-    name = models.CharField(null=False, blank=False, max_length=100)
+    name = models.CharField(
+        null=False, blank=False, max_length=100, choices=MUSCLE_GROUP
+    )
 
     # Methods
     def __str__(self):
