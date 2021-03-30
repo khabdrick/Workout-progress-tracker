22c7d1ec945c3d5c16dc76fedcade95b1f6c3899
Muhammed Ali <muhamzyali@gmail.com>
2021-03-30 13:56:40 +0100

    clean up code

--- body/migrations/0002_auto_20210330_1144.py
+++ body/migrations/0002_auto_20210330_1144.py
@@ -6,13 +6,25 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('body', '0001_initial'),
+        ("body", "0001_initial"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='musclegroup',
-            name='name',
-            field=models.CharField(choices=[('calves', 'Calves'), ('hamstrings', 'Hamstrings'), ('quads', 'Quadriceps(quads)'), ('glutes', 'Glutes'), ('biceps', 'Biceps'), ('triceps', 'Triceps'), ('forearms', 'Forearms'), ('traps', 'Trapezius(traps)')], max_length=100),
+            model_name="musclegroup",
+            name="name",
+            field=models.CharField(
+                choices=[
+                    ("calves", "Calves"),
+                    ("hamstrings", "Hamstrings"),
+                    ("quads", "Quadriceps(quads)"),
+                    ("glutes", "Glutes"),
+                    ("biceps", "Biceps"),
+                    ("triceps", "Triceps"),
+                    ("forearms", "Forearms"),
+                    ("traps", "Trapezius(traps)"),
+                ],
+                max_length=100,
+            ),
         ),
     ]
