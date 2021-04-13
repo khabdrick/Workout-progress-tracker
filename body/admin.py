598fa5e41361777733ee74f603780ec6245a0626
Muhammed Ali <muhamzyali@gmail.com>
2021-04-13 12:54:08 +0100

    deleted body app

--- body/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import MuscleGroup
-
-admin.site.register(MuscleGroup)
--- body/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BodyConfig(AppConfig):
-    name = "body"
