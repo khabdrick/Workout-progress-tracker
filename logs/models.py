28c3af58914db7218e34fcf86813260047dffb52
Muhammed Ali <muhamzyali@gmail.com>
2021-04-22 11:55:42 +0100

    updated model

--- logs/models.py
+++ logs/models.py
@@ -31,7 +31,6 @@ class WorkoutLog(models.Model):
 
     # Attributes
     day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
-
     summary = models.TextField(null=False, blank=True, max_length=1500)
     goal = models.CharField(max_length=2, choices=TYPE, default="MA")
     date = models.DateField(max_length=223, auto_now_add=True)
