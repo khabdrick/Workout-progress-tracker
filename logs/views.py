a4e68f480770a8017c99b87b854b58e5a74d4696
Muhammed Ali <muhamzyali@gmail.com>
2021-04-26 11:25:01 +0100

    log list

--- logs/views.py
+++ logs/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .forms import WorkoutLogForm
 from .models import WorkoutLog
 
@@ -13,3 +14,13 @@ class WorkoutLogCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class UserGroceryListView(ListView):
+    model = WorkoutLog
+    template_name = 'logs/workoutloglist.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'grocery'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Grocery.objects.filter(user=user).order_by('-date')
+
