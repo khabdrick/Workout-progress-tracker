4459911df0aefa3486986b0873b72ffbb106f9b4
Muhammed Ali <muhamzyali@gmail.com>
2021-04-26 11:47:40 +0100

    log list url

--- logs/urls.py
+++ logs/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import WorkoutLogCreateView, W
+from .views import WorkoutLogCreateView, WorkoutLogListView
 
 app_name = "logs"
 
 urlpatterns = [
     path("log_form/", WorkoutLogCreateView.as_view(), name="log_create"),
-    path("log_form/", WorkoutLogCreateView.as_view(), name="log_create"),
+    path("log_list/", WorkoutLogListView.as_view(), name="log_list"),
 ]
