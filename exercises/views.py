f9a5961281725deae986b8a8046b548525487cd5
Muhammed Ali <muhamzyali@gmail.com>
2021-04-29 13:49:29 +0100

    change exercise form to post method

--- exercises/views.py
+++ exercises/views.py
@@ -1,10 +1,14 @@
-from django.views.generic import FormView
+from django.views.generic import CreateView
 from .models import Exercise
 from .forms import ExerciseForm
 from django.urls import reverse_lazy
 
 
-class ExerciseCreateView(FormView):
+class ExerciseCreateView(CreateView):
+    model = Exercise
     form_class = ExerciseForm
     template_name = "exercises/exercise_form.html"
     success_url = reverse_lazy('logs:log_list')
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
