a40d3b809ca3233fed15d1f907ca5f563220d3d3
Muhammed Ali <muhamzyali@gmail.com>
2021-04-29 13:15:32 +0100

    reverse for exercise form

--- exercises/views.py
+++ exercises/views.py
@@ -1,8 +1,10 @@
 from django.views.generic import FormView
 from .models import Exercise
 from .forms import ExerciseForm
+from django.urls import reverse_lazy
 
 
 class ExerciseCreateView(FormView):
     form_class = ExerciseForm
     template_name = "exercises/exercise_form.html"
+    success_url = reverse_lazy('logs:log_list')
