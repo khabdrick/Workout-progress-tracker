d217404bd487d1e6d05cd474f2c7c17d81a562ec
Muhammed Ali <muhamzyali@gmail.com>
2021-04-29 14:06:37 +0100

    exercise form redirect to home

--- exercises/views.py
+++ exercises/views.py
@@ -1,14 +1,22 @@
 from django.views.generic import CreateView
 from .models import Exercise
 from .forms import ExerciseForm
-from django.urls import reverse_lazy
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
 
 
 class ExerciseCreateView(CreateView):
     model = Exercise
     form_class = ExerciseForm
     template_name = "exercises/exercise_form.html"
-    success_url = reverse_lazy('logs:log_list')
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("logs:log_list", args=[request.user.username]))
+        return super().get(request, *args, **kwargs)
+
+
+
