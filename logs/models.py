0b0049948ca912153301b6c206e3dead959ff30d
Muhammed Ali <muhamzyali@gmail.com>
2021-05-01 16:19:34 +0100

    updated log models

--- logs/models.py
+++ logs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.enums import Choices
 from multiselectfield import MultiSelectField
 from django.contrib.auth import get_user_model
+
 # from users.models import User
 User = get_user_model()
 
@@ -15,7 +16,6 @@ DAYS_OF_THE_WEEK = (
     ("saturday", "Saturday"),
     ("sunday", "Sunday"),
 )
-
 
 
 class WorkoutLog(models.Model):
@@ -36,12 +36,17 @@ class WorkoutLog(models.Model):
     )
 
     # Attributes
-    user = models.ForeignKey(User, related_name="userlog", on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(
+        User, related_name="userlog", on_delete=models.CASCADE, null=True, blank=True
+    )
     day = models.CharField(max_length=225, choices=DAYS_OF_THE_WEEK)
     summary = models.TextField(null=False, blank=True, max_length=1500)
     goal = models.CharField(max_length=2, choices=TYPE, default="MA")
     date = models.DateField(max_length=223, auto_now_add=True)
-    exercises = models.ManyToManyField(to="exercises.ExerciseSet", blank=True,)
+    exercises = models.ManyToManyField(
+        to="exercises.ExerciseSet",
+        blank=True,
+    )
     muscles = MultiSelectField(max_length=100, choices=MUSCLE_GROUP, default=None)
 
     # Methods
