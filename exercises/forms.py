f23ee71ff175ebd80e7dd3b53be0b635cf8caaf3
Muhammed Ali <muhamzyali@gmail.com>
2021-05-01 14:58:38 +0100

    set form file

--- exercises/forms.py
+++ exercises/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Exercise
+from .models import Exercise, Set
 
 
 class ExerciseForm(forms.ModelForm):
@@ -16,10 +16,39 @@ class ExerciseForm(forms.ModelForm):
                     "class": "form-control",
                 }
             ),
-            "tips": forms.Textarea(
+            "tips": forms.Textarea(attrs={"class": "form-control", "rows": "3"}),
+            # "muscle": forms.CheckboxSelectMultiple(
+            #     attrs={"class": "list-unstyled", "type": "checkbox"}
+            # ),
+        }
+
+
+class SetForm(forms.ModelForm):
+    class Meta:
+        fields = [
+            "reps",
+            "reps_unit",
+            "weight",
+            "weight_unit",
+        ]
+        model = Set
+        widgets = {
+            "reps": forms.TextInput(
                 attrs={
                     "class": "form-control",
-                    "rows":"3"
+                }
+            ),
+            "reps_unit": forms.Textarea(
+                attrs={
+                    "class": "list-unstyled",
+                    "type": "checkbox",
+                }
+            ),
+            "weight": forms.Textarea(attrs={"class": "form-control", "rows": "3"}),
+            "weight_unit": forms.CheckboxSelectMultiple(
+                attrs={
+                    "class": "list-unstyled",
+                    "type": "checkbox",
                 }
             ),
             # "muscle": forms.CheckboxSelectMultiple(
