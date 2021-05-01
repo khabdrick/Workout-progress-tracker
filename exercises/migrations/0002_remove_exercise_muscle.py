c65d2727723cf6aab25e4a9c2ecc59c6b123e79a
Muhammed Ali <muhamzyali@gmail.com>
2021-05-01 16:28:01 +0100

    updated exercise migrations

--- exercises/migrations/0002_remove_exercise_muscle.py
+++ exercises/migrations/0002_remove_exercise_muscle.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('exercises', '0001_initial'),
+        ("exercises", "0001_initial"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='exercise',
-            name='muscle',
+            model_name="exercise",
+            name="muscle",
         ),
     ]
