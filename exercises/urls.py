0a4ff931a47d37b21f7f7bbe217ea4a5ed4d8ad5
Muhammed Ali <muhamzyali@gmail.com>
2021-05-01 15:16:51 +0100

    set form view

--- exercises/urls.py
+++ exercises/urls.py
@@ -6,4 +6,5 @@ app_name = "exercises"
 urlpatterns = [
     path("exercise_form/", ExerciseCreateView.as_view(), name="exercise_create"),
     path("set_form/", SetCreateView.as_view(), name="set_create"),
+    
 ]
