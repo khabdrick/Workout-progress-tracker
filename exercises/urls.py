9320700f6f0aae602a5aac6b8351309cbd67f678
Muhammed Ali <muhamzyali@gmail.com>
2021-05-01 15:16:31 +0100

    set form view

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ExerciseCreateView
+from .views import ExerciseCreateView, SetCreateView
 
 app_name = "exercises"
 
 urlpatterns = [
     path("exercise_form/", ExerciseCreateView.as_view(), name="exercise_create"),
+    path("set_form/", SetCreateView.as_view(), name="set_create"),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,17 +1,30 @@
 from django.views.generic import CreateView
-from .models import Exercise
-from .forms import ExerciseForm
-from django.urls import reverse
-
+from .models import Exercise, Set
+from .forms import ExerciseForm, SetForm
+from django.urls import reverse_lazy, reverse
 
 
 class ExerciseCreateView(CreateView):
     model = Exercise
     form_class = ExerciseForm
     template_name = "exercises/exercise_form.html"
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
     def get_success_url(self):
-        return reverse('logs:log_list', args=[self.request.user.username])
-        
+        return reverse("logs:log_list", args=[self.request.user.username])
+
+
+class SetCreateView(CreateView):
+    model = Set
+    form_class = SetForm
+    template_name = "exercises/set_form.html"
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy("logs:log_create")
