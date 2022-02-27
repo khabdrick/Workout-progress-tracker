dd35f456fedeb0f3a2347ea7249876d39913766e
Muhammed Ali <muhamzyali@gmail.com>
2022-02-27 10:10:32 +0100

    url for excercise set view set

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import ExerciseCreateView, SetCreateView, ExerciseViewSet
+from .views import ExerciseCreateView, SetCreateView, ExerciseViewSet, ExerciseSetViewSet
 from rest_framework import routers
 
 app_name = "exercises"
 router = routers.DefaultRouter()
 router.register(r"exercises", ExerciseViewSet)  # route for exercises
+router.register(r"exerciseset", ExerciseSetViewSet)  # route for exercise sets
 
 
 urlpatterns = [
