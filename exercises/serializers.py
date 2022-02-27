b7bddf2b969ef13ede89ec566a20f0ac0e4ea8ae
Muhammed Ali <muhamzyali@gmail.com>
2022-02-27 09:31:04 +0100

    serializer for excercise set

--- exercises/serializers.py
+++ exercises/serializers.py
@@ -7,3 +7,15 @@ class ExerciseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Exercise
         fields = ("name", "tips")
+
+class ExerciseSetSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ExerciseSet
+
+    def to_representation(self, instance):
+        representation = dict()
+        representation["exercise"] = instance.exercise.name
+        representation["exercise"] = instance.exercise.tips
+        representation["sets"] = instance.exercises.Set
+        representation["number_of_sets"] = instance.number_of_sets
