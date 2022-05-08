7574d7e1325c7602b373aa0474683f3f4aabb548
Muhammed Ali <muhamzyali@gmail.com>
2022-05-08 13:58:52 +0100

    update model

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-class 
+class BodyBuilderQuote(models.Model):
+    name = models.CharField(max_length=200)
+    quote = models.CharField(max_length=1000)
+
+    # Methods
+    def __str__(self):
+        return self.name
+
+class FitnessDidYouKnowQuote(models.Model):
+    quote = models.CharField(max_length=1000)
+
+    # Methods
+    def __str__(self):
+        return self.quote
